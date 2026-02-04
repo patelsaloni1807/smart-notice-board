87d7daaedf8051919e48a0d4e9c09f5fa4ebe6fb
patelsaloni1807 <patelsalu1807@gmail.com>
2026-02-04 11:33:15 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,15 +1,17 @@
 module.exports = {
-  ensureAuthenticated: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
+    ensureAuthenticated: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        req.flash('error_msg', 'Please log in to view that resource');
+        res.redirect('/auth/login');
+    },
+    ensureAdmin: function (req, res, next) {
+        if (req.isAuthenticated() && req.user.role === 'admin') {
+            return next();
+        }
+        console.log('Access denied. User role:', req.user ? req.user.role : 'not logged in');
+        req.flash('error_msg', 'Access denied. Admin privileges required.');
+        res.redirect('/');
     }
-    res.redirect("/auth/login");
-  },
-
-  ensureAdmin: function (req, res, next) {
-    if (req.user && req.user.role === "admin") {
-      return next();
-    }
-    res.redirect("/");
-  },
 };
